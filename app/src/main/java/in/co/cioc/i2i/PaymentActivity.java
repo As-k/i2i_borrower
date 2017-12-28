96ccaaf758ec6a9bde5fa7a183b6823c601ac3bb
pradeep <pradeep@cioc.co.in>
2017-12-28 16:21:33 +0530

    payment and company search

---
+++ app/src/main/java/in/co/cioc/i2i/PaymentActivity.java
@@ -0,0 +1,292 @@
+package in.co.cioc.i2i;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.payu.india.Extras.PayUChecksum;
+import com.payu.india.Extras.PayUSdkDetails;
+import com.payu.india.Model.PaymentParams;
+import com.payu.india.Model.PayuConfig;
+import com.payu.india.Model.PayuHashes;
+import com.payu.india.Model.PostData;
+import com.payu.india.Payu.Payu;
+import com.payu.india.Payu.PayuConstants;
+import com.payu.india.Payu.PayuErrors;
+import com.payu.payuui.Activity.PayUBaseActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+public class PaymentActivity extends AppCompatActivity {
+
+    private Button payBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        Payu.setInstance(this);
+
+
+    }
+
+
+    private String merchantKey = "r9pHRt";
+    private String userCredentials;
+
+    // These will hold all the payment parameters
+    private PaymentParams mPaymentParams;
+
+    // This sets the configuration
+    private PayuConfig payuConfig;
+
+
+    // Used when generating hash from SDK
+    private PayUChecksum checksum;
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+        if (requestCode == PayuConstants.PAYU_REQUEST_CODE) {
+            if (data != null) {
+
+                /**
+                 * Here, data.getStringExtra("payu_response") ---> Implicit response sent by PayU
+                 * data.getStringExtra("result") ---> Response received from merchant's Surl/Furl
+                 *
+                 * PayU sends the same response to merchant server and in app. In response check the value of key "status"
+                 * for identifying status of transaction. There are two possible status like, success or failure
+                 * */
+                new AlertDialog.Builder(this)
+                        .setCancelable(false)
+                        .setMessage("Payu's Data : " + data.getStringExtra("payu_response") + "\n\n\n Merchant's Data: " + data.getStringExtra("result"))
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+
+            } else {
+                Toast.makeText(this, getString(R.string.could_not_receive_data), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    /**
+     * This method prepares all the payments params to be sent to PayuBaseActivity.java
+     */
+    public void navigateToBaseActivity(View view) {
+
+
+        // merchantKey="";
+        String amount = "1";
+        String email = "pkyisky@gmail.com";
+
+        userCredentials = merchantKey + ":" + email;
+
+        //TODO Below are mandatory params for hash genetation
+        mPaymentParams = new PaymentParams();
+        /**
+         * For Test Environment, merchantKey = please contact mobile.integration@payu.in with your app name and registered email id
+
+         */
+        mPaymentParams.setKey(merchantKey);
+        mPaymentParams.setAmount(amount);
+        mPaymentParams.setProductInfo("product_info");
+        mPaymentParams.setFirstName("firstname");
+        mPaymentParams.setEmail("test@gmail.com");
+        mPaymentParams.setPhone("9876543210");
+
+
+        /*
+        * Transaction Id should be kept unique for each transaction.
+        * */
+        mPaymentParams.setTxnId("" + System.currentTimeMillis());
+
+        /**
+         * Surl --> Success url is where the transaction response is posted by PayU on successful transaction
+         * Furl --> Failre url is where the transaction response is posted by PayU on failed transaction
+         */
+        mPaymentParams.setSurl("https://payu.herokuapp.com/success");
+        mPaymentParams.setFurl("https://payu.herokuapp.com/failure");
+        mPaymentParams.setNotifyURL(mPaymentParams.getSurl());  //for lazy pay
+
+        /*
+         * udf1 to udf5 are options params where you can pass additional information related to transaction.
+         * If you don't want to use it, then send them as empty string like, udf1=""
+         * */
+        mPaymentParams.setUdf1("udf1");
+        mPaymentParams.setUdf2("udf2");
+        mPaymentParams.setUdf3("udf3");
+        mPaymentParams.setUdf4("udf4");
+        mPaymentParams.setUdf5("udf5");
+
+        /**
+         * These are used for store card feature. If you are not using it then user_credentials = "default"
+         * user_credentials takes of the form like user_credentials = "merchant_key : user_id"
+         * here merchant_key = your merchant key,
+         * user_id = unique id related to user like, email, phone number, etc.
+         * */
+        mPaymentParams.setUserCredentials(userCredentials);
+
+        //TODO Pass this param only if using offer key
+        //mPaymentParams.setOfferKey("cardnumber@8370");
+
+        //TODO Sets the payment environment in PayuConfig object
+        payuConfig = new PayuConfig();
+        payuConfig.setEnvironment(PayuConstants.PRODUCTION_ENV);
+        //   payuConfig.setEnvironment(PayuConstants.MOBILE_STAGING_ENV);
+        //TODO It is recommended to generate hash from server only. Keep your key and salt in server side hash generation code.
+//        generateHashFromServer(mPaymentParams);
+
+        /**
+         * Below approach for generating hash is not recommended. However, this approach can be used to test in PRODUCTION_ENV
+         * if your server side hash generation code is not completely setup. While going live this approach for hash generation
+         * should not be used.
+         * */
+        String salt = "YVj4MmV1";
+        generateHashFromSDK(mPaymentParams, salt);
+
+    }
+
+    /******************************
+     * Client hash generation
+     ***********************************/
+    // Do not use this, you may use this only for testing.
+    // lets generate hashes.
+    // This should be done from server side..
+    // Do not keep salt anywhere in app.
+    public void generateHashFromSDK(PaymentParams mPaymentParams, String salt) {
+        PayuHashes payuHashes = new PayuHashes();
+        PostData postData = new PostData();
+
+        // payment Hash;
+        checksum = null;
+        checksum = new PayUChecksum();
+        checksum.setAmount(mPaymentParams.getAmount());
+        checksum.setKey(mPaymentParams.getKey());
+        checksum.setTxnid(mPaymentParams.getTxnId());
+        checksum.setEmail(mPaymentParams.getEmail());
+        checksum.setSalt(salt);
+        checksum.setProductinfo(mPaymentParams.getProductInfo());
+        checksum.setFirstname(mPaymentParams.getFirstName());
+        checksum.setUdf1(mPaymentParams.getUdf1());
+        checksum.setUdf2(mPaymentParams.getUdf2());
+        checksum.setUdf3(mPaymentParams.getUdf3());
+        checksum.setUdf4(mPaymentParams.getUdf4());
+        checksum.setUdf5(mPaymentParams.getUdf5());
+
+        postData = checksum.getHash();
+        if (postData.getCode() == PayuErrors.NO_ERROR) {
+            payuHashes.setPaymentHash(postData.getResult());
+        }
+
+        // checksum for payemnt related details
+        // var1 should be either user credentials or default
+        String var1 = mPaymentParams.getUserCredentials() == null ? PayuConstants.DEFAULT : mPaymentParams.getUserCredentials();
+        String key = mPaymentParams.getKey();
+
+        if ((postData = calculateHash(key, PayuConstants.PAYMENT_RELATED_DETAILS_FOR_MOBILE_SDK, var1, salt)) != null && postData.getCode() == PayuErrors.NO_ERROR) // Assign post data first then check for success
+            payuHashes.setPaymentRelatedDetailsForMobileSdkHash(postData.getResult());
+        //vas
+        if ((postData = calculateHash(key, PayuConstants.VAS_FOR_MOBILE_SDK, PayuConstants.DEFAULT, salt)) != null && postData.getCode() == PayuErrors.NO_ERROR)
+            payuHashes.setVasForMobileSdkHash(postData.getResult());
+
+        // getIbibocodes
+        if ((postData = calculateHash(key, PayuConstants.GET_MERCHANT_IBIBO_CODES, PayuConstants.DEFAULT, salt)) != null && postData.getCode() == PayuErrors.NO_ERROR)
+            payuHashes.setMerchantIbiboCodesHash(postData.getResult());
+
+        if (!var1.contentEquals(PayuConstants.DEFAULT)) {
+            // get user card
+            if ((postData = calculateHash(key, PayuConstants.GET_USER_CARDS, var1, salt)) != null && postData.getCode() == PayuErrors.NO_ERROR) // todo rename storedc ard
+                payuHashes.setStoredCardsHash(postData.getResult());
+            // save user card
+            if ((postData = calculateHash(key, PayuConstants.SAVE_USER_CARD, var1, salt)) != null && postData.getCode() == PayuErrors.NO_ERROR)
+                payuHashes.setSaveCardHash(postData.getResult());
+            // delete user card
+            if ((postData = calculateHash(key, PayuConstants.DELETE_USER_CARD, var1, salt)) != null && postData.getCode() == PayuErrors.NO_ERROR)
+                payuHashes.setDeleteCardHash(postData.getResult());
+            // edit user card
+            if ((postData = calculateHash(key, PayuConstants.EDIT_USER_CARD, var1, salt)) != null && postData.getCode() == PayuErrors.NO_ERROR)
+                payuHashes.setEditCardHash(postData.getResult());
+        }
+
+        if (mPaymentParams.getOfferKey() != null) {
+            postData = calculateHash(key, PayuConstants.OFFER_KEY, mPaymentParams.getOfferKey(), salt);
+            if (postData.getCode() == PayuErrors.NO_ERROR) {
+                payuHashes.setCheckOfferStatusHash(postData.getResult());
+            }
+        }
+
+        if (mPaymentParams.getOfferKey() != null && (postData = calculateHash(key, PayuConstants.CHECK_OFFER_STATUS, mPaymentParams.getOfferKey(), salt)) != null && postData.getCode() == PayuErrors.NO_ERROR) {
+            payuHashes.setCheckOfferStatusHash(postData.getResult());
+        }
+
+        // we have generated all the hases now lest launch sdk's ui
+        launchSdkUI(payuHashes);
+    }
+
+    // deprecated, should be used only for testing.
+    private PostData calculateHash(String key, String command, String var1, String salt) {
+        checksum = null;
+        checksum = new PayUChecksum();
+        checksum.setKey(key);
+        checksum.setCommand(command);
+        checksum.setVar1(var1);
+        checksum.setSalt(salt);
+        return checksum.getHash();
+    }
+
+
+    /**
+     * This method adds the Payuhashes and other required params to intent and launches the PayuBaseActivity.java
+     *
+     * @param payuHashes it contains all the hashes generated from merchant server
+     */
+    public void launchSdkUI(PayuHashes payuHashes) {
+
+        Intent intent = new Intent(this, PayUBaseActivity.class);
+        intent.putExtra(PayuConstants.PAYU_CONFIG, payuConfig);
+        intent.putExtra(PayuConstants.PAYMENT_PARAMS, mPaymentParams);
+        intent.putExtra(PayuConstants.PAYU_HASHES, payuHashes);
+
+        //Lets fetch all the one click card tokens first
+        startActivityForResult(intent , PayuConstants.PAYU_REQUEST_CODE);
+
+    }
+
+
+
+
+
+}
--- app/src/main/java/in/co/cioc/i2i/RegistrationCheckEligibility.java
+++ app/src/main/java/in/co/cioc/i2i/RegistrationCheckEligibility.java
@@ -2,14 +2,17 @@ package in.co.cioc.i2i;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -30,7 +33,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class RegistrationCheckEligibility extends AppCompatActivity {
 
-
+    Boolean loading;
     private static AsyncHttpClient client = new AsyncHttpClient();
     Backend backend;
     Spinner dropdownPurpose , spinnerPaymentMode;
@@ -38,6 +41,18 @@ public class RegistrationCheckEligibility extends AppCompatActivity {
     private Calendar calendar;
     private EditText bodEditTxt, pincodeEditTxt , cityEditTxt, workingSinceEditTxt;
     Drawable successTick;
+
+    EditText amountTxt , descriptionTxt;
+
+    EditText professionTypeTxt, turnoverSelfEmpTxt , profitSelfEmpTxt;
+    EditText professionBusinessTxt, turnoverBusinessTxt , profitBusinessTxt;
+
+    EditText incomeSalariedTxt , workingSinceTxt, workExperienceTxt , workExperienceMonthTxt;
+    AutoCompleteTextView companyTxt;
+
+    LinearLayout loanEmiLayout , ccOutstandingLayout , rentForm , spouseIncomeLayout;
+
+    EditText loanEmiTxt , ccOutstandingTxt, creditScoreTxt;
 
     public void onRadioButtonClickedMarital(View view) {
         // Is the button now checked?
@@ -71,6 +86,14 @@ public class RegistrationCheckEligibility extends AppCompatActivity {
     @Override
     protected Dialog onCreateDialog(int id) {
         // TODO Auto-generated method stub
+
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+
         if (id == 999) {
             return new DatePickerDialog(this,
                     myDateListener, year, month, day);
@@ -126,7 +149,59 @@ public class RegistrationCheckEligibility extends AppCompatActivity {
         insertIntoDP(423, "$32423", "432423" , "432423");
 
         bodEditTxt = findViewById(R.id.dob);
+
         workingSinceEditTxt = findViewById(R.id.workingSince);
+        incomeSalariedTxt = findViewById(R.id.incomeSalaried);
+        workExperienceTxt = findViewById(R.id.workExperience);
+        workExperienceMonthTxt = findViewById(R.id.workExperienceMonth);
+        companyTxt = findViewById(R.id.company);
+        companyTxt.setAdapter(new CompanyAutoCompleteAdapter(this,companyTxt.getText().toString()));
+
+        amountTxt = findViewById(R.id.amount);
+        descriptionTxt = findViewById(R.id.description);
+
+        amountTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                Integer i = Integer.parseInt(s.toString());
+
+                if(i<25000 || i > 300000){
+                    removeSuccess(amountTxt);
+                    amountTxt.setError("Amount can not be less then 25000 or greater than 300000");
+                    amountTxt.requestFocus();
+                }else if(i%5000 != 0){
+                    amountTxt.setError("Please enter amount in the multiples of 5000");
+                    amountTxt.requestFocus();
+                } else {
+                    showSuccess(amountTxt);
+                }
+            }
+        });
+
+        descriptionTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if(s.length() != 0){
+                    showSuccess(descriptionTxt);
+                }else {
+                    removeSuccess(descriptionTxt);
+                    descriptionTxt.setError("Please explain why you need this loan");
+                    descriptionTxt.requestFocus();
+                }
+            }
+        });
+
+
 
         calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
@@ -154,34 +229,72 @@ public class RegistrationCheckEligibility extends AppCompatActivity {
         ArrayAdapter<String> adapterHouseType = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsHouseType);
         spinnerHouseType.setAdapter(adapterHouseType);
 
-        ///api/v1/loanPurpose
-
         final LinearLayout selfEmpForm = findViewById(R.id.emp_form_selfEmp);
         final LinearLayout businessForm = findViewById(R.id.emp_form_business);
         final LinearLayout salariedForm = findViewById(R.id.emp_form_salaried);
 
+        spouseIncomeLayout = findViewById(R.id.spouseIncomeLayout);
+        spouseIncomeLayout.setVisibility(LinearLayout.GONE);
+
         selfEmpForm.setVisibility(LinearLayout.GONE);
         businessForm.setVisibility(LinearLayout.GONE);
 
+        professionTypeTxt = findViewById(R.id.professionSelfEmp);
+        turnoverSelfEmpTxt = findViewById(R.id.turnoverSelfEmp);
+        profitSelfEmpTxt = findViewById(R.id.profitSelfEmp);
+
+        professionBusinessTxt = findViewById(R.id.professionBusiness);
+        turnoverBusinessTxt = findViewById(R.id.turnoverBusiness);
+        profitBusinessTxt = findViewById(R.id.profitBusiness);
+
+        loanEmiTxt = findViewById(R.id.loanEmi);
+        ccOutstandingTxt = findViewById(R.id.ccOutstanding);
+        creditScoreTxt = findViewById(R.id.creditScore);
+
+        loading = true;
+
+        spinnerHouseType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0){
+                    rentForm.setVisibility(LinearLayout.VISIBLE);
+                }else{
+                    rentForm.setVisibility(LinearLayout.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         dropdownEmpType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 // your code here
+
                 if (position == 0){
                     salariedForm.setVisibility(LinearLayout.VISIBLE);
                     selfEmpForm.setVisibility(LinearLayout.GONE);
                     businessForm.setVisibility(LinearLayout.GONE);
+                    if (!loading){
+                        companyTxt.requestFocus();
+                    }
+
                 }else if (position == 1){
                     salariedForm.setVisibility(LinearLayout.GONE);
                     selfEmpForm.setVisibility(LinearLayout.VISIBLE);
                     businessForm.setVisibility(LinearLayout.GONE);
+                    professionTypeTxt.requestFocus();
 
                 }else if (position == 2){
                     salariedForm.setVisibility(LinearLayout.GONE);
                     selfEmpForm.setVisibility(LinearLayout.GONE);
                     businessForm.setVisibility(LinearLayout.VISIBLE);
+                    professionBusinessTxt.requestFocus();
                 }
+                loading = false;
             }
 
             @Override
@@ -192,6 +305,7 @@ public class RegistrationCheckEligibility extends AppCompatActivity {
         });
 
         dropdownPurpose = findViewById(R.id.spinnerPurpose);
+
 
 
 
@@ -234,7 +348,13 @@ public class RegistrationCheckEligibility extends AppCompatActivity {
         int w = successTick.getIntrinsicWidth();
         successTick.setBounds( 0, 0, w, h );
 
+        ccOutstandingLayout = findViewById(R.id.ccOutstandingLayout);
+        loanEmiLayout = findViewById(R.id.loanEmiLayout);
+        ccOutstandingLayout.setVisibility(LinearLayout.GONE);
+        loanEmiLayout.setVisibility(LinearLayout.GONE);
 
+        rentForm = findViewById(R.id.rentForm);
+        rentForm.setVisibility(LinearLayout.GONE);
 
         pincodeEditTxt = findViewById(R.id.pincode);
         cityEditTxt = findViewById(R.id.city);
@@ -283,8 +403,43 @@ public class RegistrationCheckEligibility extends AppCompatActivity {
         });
 
 
+    }
 
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
 
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_yes_creditCard:
+                if (checked)
+                    ccOutstandingLayout.setVisibility(LinearLayout.VISIBLE);
+                    ccOutstandingTxt.requestFocus();
+                    break;
+            case R.id.radio_no_creditCard:
+                if (checked)
+                    creditScoreTxt.requestFocus();
+                    ccOutstandingLayout.setVisibility(LinearLayout.GONE);
+                    break;
+            case R.id.radio_yes_loan:
+                if (checked)
+                    loanEmiLayout.setVisibility(LinearLayout.VISIBLE);
+                    loanEmiTxt.requestFocus();
+                break;
+            case R.id.radio_no_loan:
+                if (checked)
+                    creditScoreTxt.requestFocus();
+                    loanEmiLayout.setVisibility(LinearLayout.GONE);
+                break;
+            case R.id.radio_single:
+                if (checked)
+                    spouseIncomeLayout.setVisibility(LinearLayout.GONE);
+                break;
+            case R.id.radio_married:
+                if (checked)
+                    spouseIncomeLayout.setVisibility(LinearLayout.VISIBLE);
+                break;
+        }
     }
 
     public void showSuccess(EditText edit){
